431923e87d8d49411e38d95eb771efff1430f0db
joseph hill <hill@hoare7.cs.umsl.edu>
2019-09-07 20:14:59 -0500

    modified:   a.out 	new file:   link1 	new file:   link2 	modified:   main.c

--- hill/testdir/main.c
+++ hill/testdir/main.c
@@ -3,12 +3,145 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <stdbool.h>
+#include <time.h>
 
 char * getFile(char[]);
-
-int level = 0;
+bool I = false, L = false, t = false, p = false, i = false, u = false, g = false, s = false, d = false, l = false;
+int level = 0, indent = 3;
 char finalPath[100] = "";
+
+void display(char file[], char currentPath[], struct stat str){
+
+    char fileWithSpacing[100] = "" , tempFile[100];
+
+    if(I == true){
+        int x;
+        for(x = indent; x != 0; x--){
+            int tempLevel;
+            for(tempLevel = level; tempLevel != 0; tempLevel--) {
+                strcat(fileWithSpacing, " ");
+            }
+        }
+        strcpy(tempFile, file);
+        strcat(fileWithSpacing, tempFile);
+        printf("%-20s", fileWithSpacing);
+    }
+    else{
+        int tempLevel;
+        for(tempLevel = level; tempLevel != 0; tempLevel--) {
+            strcat(fileWithSpacing, "   ");
+        }
+        strcpy(tempFile, file);
+        strcat(fileWithSpacing, tempFile);
+        printf("%-20s", fileWithSpacing);
+    }
+    if(l == true){
+        t = true;
+        p = true;
+        i = true;
+        u = true;
+        g = true;
+        s = true;
+    }
+    if(L == true){
+        if(str.st_mode & S_IFLNK){
+            char *sympath;
+            if(lstat(currentPath, &str) == 0) {
+                sympath = realpath(currentPath, NULL);
+                printf("Path of Link is %s\n", sympath);
+            }
+        }
+    }
+    if(d == true){
+        if(stat(currentPath, &str) == 0){
+            char time[100];
+            strcpy(time, ctime(&str.st_mtime));
+            int timeLen = strlen(time);
+            if(time[timeLen-1] == '\n'){
+                time[timeLen-1] = 0;
+            }
+            printf("%-26s ", time);
+        }
+    }
+    if(g == true){
+        printf("%-5ld ", getgid());
+    }
+    if(i == true){
+        if(stat(currentPath, &str) == 0) {
+            printf("%-3ld ", (long) str.st_nlink);
+        }
+    }
+    if(p == true){
+        char mode[10] = "";
+
+        if(stat(currentPath, &str) == 0){
+            mode_t permissions = str.st_mode;
+            mode[0] = (permissions & S_IRUSR) ? 'r' : '-';
+            mode[1] = (permissions & S_IWUSR) ? 'w' : '-';
+            mode[2] = (permissions & S_IXUSR) ? 'x' : '-';
+            mode[3] = (permissions & S_IRGRP) ? 'r' : '-';
+            mode[4] = (permissions & S_IWGRP) ? 'w' : '-';
+            mode[5] = (permissions & S_IXGRP) ? 'x' : '-';
+            mode[6] = (permissions & S_IROTH) ? 'r' : '-';
+            mode[7] = (permissions & S_IWOTH) ? 'w' : '-';
+            mode[8] = (permissions & S_IXOTH) ? 'x' : '-';
+            mode[9] = '\0';
+        }
+        printf("%-11s", mode);
+    }
+    if(s == true){
+        char type[2] = "";
+        char fullSize[10];
+        long size;
+        if(stat(currentPath, &str) == 0) {
+            size = (long) str.st_size;
+            if(size >= 1000){
+                size = size / 1000;
+                if (size >= 1000){
+                    size = size / 1000;
+                    if(size >= 1000){
+                        size = size / 1000;
+                        strcpy(type, "G");
+                    }
+                    else{
+                        strcpy(type, "M");
+                    }
+                }
+                else{
+                    strcpy(type, "K");
+                }
+            }
+        }
+        sprintf(fullSize, "%d", size);
+        strcat(fullSize, type);
+        printf("%-6s", fullSize);
+    }
+    if(t == true){
+        char fileType[15];
+
+        switch (str.st_mode & S_IFMT) {
+            case S_IFBLK:  strcpy(fileType, "block");                   break;
+            case S_IFCHR:  strcpy(fileType, "character device");        break;
+            case S_IFDIR:  strcpy(fileType, "directory");               break;
+            case S_IFIFO:  strcpy(fileType, "FIFO/pipe");               break;
+            case S_IFLNK:  strcpy(fileType, "symlink");                 break;
+            case S_IFREG:  strcpy(fileType, "regular file");            break;
+            case S_IFSOCK: strcpy(fileType, "socket");                  break;
+            default:       strcpy(fileType, "unknown");                 break;
+        }
+
+        printf("%-20s", fileType);
+    }
+    if(u == true){
+        printf("%-6ld ", getuid());
+    }
+
+    printf("\n");
+
+}
 
 char * getPath(){
     char buffer[100];
@@ -96,19 +229,17 @@ void checkStat(char file[]){
     }
     strcat(currentPath, "/");
     strcat(currentPath, file);
-    //printf("%s is current path \n", currentPath);
     struct stat s;
 
     if(stat(currentPath, &s) == 0){
 
         int tempLevel = level;
         while(tempLevel != 0){
-            printf("   ");
             tempLevel--;
         }
         if(s.st_mode & S_IFDIR) {
             char levelName[100], newfile[100];
-            printf("%s\n", file);
+            display(file, currentPath, s);
             chdir(currentPath);
             level++;
             sprintf(levelName, "%d", level);
@@ -119,7 +250,7 @@ void checkStat(char file[]){
             level--;
         }
         else if(s.st_mode & S_IFREG){
-            printf("%s\n", file);
+            display(file, currentPath, s);
         }
         else{
             printf("We don't know wtf this is \n");
@@ -147,31 +278,88 @@ void control(){
     char path[100];
 
     strcpy(path, getPath());
-    printf("%s", path); //lists current path
 
     sprintf(levelName, "%d", level);
     makeLsFile();
     strcpy(buffer, getFile(levelName));
     checkStat(buffer);
-    system("pwd");
     remove("0");
 
-
-
-    /*for(i = level; i != -1; i--){
-        char this[100];
-        sprintf(this, "%d", i);
-        remove(this);
-    }*/
 }
 
-int main( int argc, const char* argv[] )
+int main( int argc, char *argv[] )
 {
-    int i;
-    for( i = 0; i < argc; i++) {
-        printf("%s \n", argv[i]);   //lists commandline arguments
-    }
 
+    int opt = 0;
+    while ((opt = getopt(argc, argv,"hI:Ldgipstul")) != -1) {
+        switch (opt) {
+            case 'h' : printf("Display help \n");
+                printf("List of arguments:\n");
+                printf("-h -> Print a help message and exit.\n");
+                printf("I n -> Change indentation to n spaces for each level.\n");
+                printf("L -> Follow symbolic links, if any. Default will be to not follow symbolic links.\n");
+                printf("t -> Print information on file type.\n");
+                printf("p -> Print permission bits as rwxrwxrwx.\n");
+                printf("i -> Print the number of links to file in inode table.\n");
+                printf("u -> Print the uid associated with the file.\n");
+                printf("g -> Print the gid associated with the file.\n");
+                break;
+            case 'I' :
+                I = true;
+                indent = atoi(optarg);
+                break;
+            case ':' :
+                break;
+            case 'L' :
+                L = true;
+                break;
+            case 'd' :
+                d = true;
+                break;
+            case 'g' :
+                g = true;
+                break;
+            case 'i' :
+                i = true;
+                break;
+            case 'p' :
+                p = true;
+                break;
+            case 's' :
+                s = true;
+                break;
+            case 't' :
+                t = true;
+                break;
+            case 'u' :
+                u = true;
+                break;
+            case 'l' :
+                l = true;
+                break;
+            default:
+                exit(EXIT_FAILURE);
+        }
+    }
+    /*
+    if(argc > 1) {
+        struct stat testStat;
+        char testPath[1000];
+        int testLen;
+        strcpy(testPath, getPath());
+        testLen = strlen(testPath);
+        if(testPath[testLen-1] == '\n'){
+            testPath[testLen-1] = 0;
+        }
+        strcat(testPath, "/");
+        strcat(testPath, argv[optind]);
+        if(stat(testPath, &testStat) == 0){
+            if(testStat.st_mode & S_IFDIR){
+                chdir(testPath);
+            }
+        }
+    }
+     */
     control();
 
     remove("finalPath.txt");
